4c5238fadfed7e37b729754dbfd262e3d267c766
zhangfangbiao <853801289@qq.com>
2019-06-05 21:20:52 +0800

    '引入vant，优化变量命名'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,10 @@
 <template>
   <div>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
-    <h1>首页</h1>
+    <van-swipe :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
   </div>
 </template>
