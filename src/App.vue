e3a4fd872bcff8ded54ddc3b5ec5397b51807e2b
Your Name <email@example.com>
2019-06-05 20:54:31 +0800

    完成路由配置和基本页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,56 @@
 <template>
   <div id="app">
-    
+    <header>
+      <router-link :to="{name:'H'}">个人中心</router-link>
+      <router-link :to="{name:'P'}">首页</router-link>
+      <router-link :to="{name:'V'}">视频</router-link>
+    </header>
+    <div class="main">
+      <router-view></router-view>
+    </div>
+    <footer>音乐播放位置</footer>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "./components/Home";
+import Personal from "./components/Personal";
+import Viode from "./components/Viode";
 export default {
-  name: 'app',
+  name: "app",
   components: {
+    //所有的组件
     // HelloWorld
+    Home,
+    Personal,
+    Viode
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#app header {
+  height: 60px;
+  width: 100%;
+  background: linear-gradient(rgb(57, 154, 184), rgb(56, 231, 222));
+  flex-shrink: 0;
+}
+#app footer {
+  height: 60px;
+  width: 100%;
+  background: linear-gradient(rgb(81, 175, 167), rgb(67, 155, 75));
+  flex-shrink: 0;
+}
+.main {
+  flex: 1;
+  overflow: auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import Home from "./components/Home.vue";
+import Viode from "./components/Viode.vue";
+import Personal from "./components/Personal.vue";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: "/",
+      component: Home,
+      name: "H"
+    },
+    {
+      path: "/Viode",
+      component: Viode,
+      name: "V"
+    },
+    {
+      path: "/Personal",
+      component: Personal,
+      name: "P"
+    }
+  ]
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  router,
+  render: h => h(App)
+}).$mount("#app");
