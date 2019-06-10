598acf739b0621fd8b2552c09bcabb055addbbb8
zhangfangbiao <853801289@qq.com>
2019-06-10 08:11:24 +0800

    'a'

--- src/components/Paihangbang.vue
+++ src/components/Paihangbang.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="paihangbangwrap">
-    <h1>排行榜</h1>
+    <div class="paihangbanghead">BangMusic排行榜</div>
+    <div class="paihangbangmain">排行榜主要内容</div>
+    <footer>
+      <router-link :to="{name:'playmusic'}">音乐播放位置</router-link>
+    </footer>
   </div>
 </template>
 <style>
@@ -8,3 +12,18 @@
   margin-top: -60px;
 }
 </style>
+<script>
+import { paihangbangreturn } from "./../serves/music";
+export default {
+  data() {
+    return {
+      list: []
+    };
+  },
+  async created() {
+    const result = await paihangbangreturn();
+    //     this.pics = result.data.banners;
+    console.log(result.data.privileges);
+  }
+};
+</script>
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,19 +1,24 @@
 import axios from "axios";
 //登录请求函数
 export function denglu(url, data) {
-  return axios.get(url, data);
+      return axios.get(url, data);
 }
 //轮播图函数
 export function lunbo(url, data) {
-  return axios.get(url, data);
+      return axios.get(url, data);
 }
 
 //我喜欢的歌曲列表
 export function likemusiclist(url, data) {
-  return axios.get(url, data);
+      return axios.get(url, data);
 }
 
 //我收藏的MV
 export function collect(url, data) {
-  return axios.get(url, data);
+      return axios.get(url, data);
+}
+
+//排行榜
+export function paihangbang(url, data) {
+      return axios.get(url, data)
 }
