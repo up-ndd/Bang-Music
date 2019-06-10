a8bf81292feec881bb1ff71e156cf85931c787db
zhangfangbiao <853801289@qq.com>
2019-06-10 12:12:17 +0800

    '排行榜'

--- src/components/Paihangbang.vue
+++ src/components/Paihangbang.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="paihangbangwrap">
     <div class="paihangbanghead">BangMusic排行榜</div>
-    <div class="paihangbangmain">排行榜主要内容</div>
+    <div class="paihangbangmain">
+      <ul>
+        <li v-for="(p,i) in list" :key="i">{{(i+1)}}：{{p.name}}</li>
+      </ul>
+    </div>
     <footer>
       <router-link :to="{name:'playmusic'}">音乐播放位置</router-link>
     </footer>
@@ -22,8 +26,23 @@ export default {
   },
   async created() {
     const result = await paihangbangreturn();
-    //     this.pics = result.data.banners;
-    console.log(result.data.privileges);
+    //     console.log(result);
+    this.list = result.data.playlist.tracks;
+    console.log(this.list);
   }
 };
 </script>
+<style>
+.paihangbanghead {
+  height: 60px;
+  background: linear-gradient(rgb(57, 154, 184), rgb(56, 231, 222));
+  line-height: 60px;
+  text-indent: 5px;
+}
+li {
+  list-style: none;
+  text-indent: 20px;
+  border-bottom: 1px dashed rgb(109, 179, 179);
+  padding: 10px 0;
+}
+</style>
