e357b962a2cbc0def61841a61710bca98cc20900
zhangfangbiao <853801289@qq.com>
2019-06-10 15:26:13 +0800

    '改变请求地址为本地服务器地址'

--- src/services/music.js
+++ src/services/music.js
@@ -1,48 +1,48 @@
 //导入函数，然后传入参数再导出
 import {
-  denglu,
-  lunbo,
-  likemusiclist,
-  collect,
-  paihangbang,
-  likedatile,
-  colloecdatile
+      denglu,
+      lunbo,
+      likemusiclist,
+      collect,
+      paihangbang,
+      likedatile,
+      colloecdatile
 } from "./../utils/request";
 
 //导出登录返回值
 export function denglureturn() {
-  return denglu(
-    "https://miximusic.herokuapp.com/login/cellphone?phone=18639389097&password=xiaofang"
-  );
+      return denglu(
+            "http://10.8.155.18:3000/login/cellphone?phone=18639389097&password=xiaofang"
+      );
 }
 //导出轮播图返回值
 export function lunboreturn() {
-  return lunbo("https://miximusic.herokuapp.com/banner?type=2");
+      return lunbo("http://10.8.155.18:3000/banner?type=2");
 }
 
 //导出我喜欢的音乐列表返回值
 export function likelistreturn() {
-  return likemusiclist(
-    "https://miximusic.herokuapp.com/likelist?uid=1553202823"
-  );
+      return likemusiclist(
+            "http://10.8.155.18:3000/likelist?uid=1553202823"
+      );
 }
 
 //歌曲详情
 export function likedatilereturn(ids) {
-  return likedatile("https://miximusic.herokuapp.com/song/detail?ids=" + ids);
+      return likedatile("http://10.8.155.18:3000/song/detail?ids=" + ids);
 }
 
 //导出我收藏的MV
 export function collectreturn() {
-  return collect("https://miximusic.herokuapp.com/mv/sublist");
+      return collect("http://10.8.155.18:3000/mv/sublist");
 }
 
 //mv详情
 export function colloecdatilereturn(id) {
-  return colloecdatile("https://miximusic.herokuapp.com/mv/detail?mvid=" + id);
+      return colloecdatile("http://10.8.155.18:3000/mv/detail?mvid=" + id);
 }
 
 //导出排行榜返回数据
 export function paihangbangreturn() {
-  return paihangbang("https://miximusic.herokuapp.com/top/list?idx=1");
+      return paihangbang("http://10.8.155.18:3000/top/list?idx=1");
 }
