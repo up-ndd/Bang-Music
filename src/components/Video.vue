d2efca5c00e37f18d08d2ce4c2dda428cd2e9d90
lizhiming <1027407764@qq.com>
2019-06-12 11:47:13 +0800

    .

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -47,23 +47,17 @@ export default {
   }
 };
 </script>
-<<<<<<< HEAD
+
 <style scoped>
-=======
-<<<<<<< HEAD
-<style scoped>
-=======
-<style>
->>>>>>> 28683e676065603fc4bc166ed006ebaab44e3a39
 * {
   margin: 0;
   padding: 0;
 }
->>>>>>> 4ea9409731f1107b7170533f71882bf03993803c
+
 img {
   width: 90%;
   height: 10rem;
-  margin: 0.5rem auto;
+  margin: 0.5rem 1rem;
 }
 </style>
 
