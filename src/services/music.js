993db77888d99eb6a4d2701896b59fae8ecbe486
lizhiming <1027407764@qq.com>
2019-06-18 15:39:10 +0800

    video尝试播放

--- src/services/music.js
+++ src/services/music.js
@@ -3,7 +3,6 @@ import { get } from "./../utils/request";
 
 //导出登录返回值
 export function denglureturn() {
-<<<<<<< HEAD
     return get(
         "http://10.8.155.18:3000/login/cellphone?phone=18639389097&password=xiaofang"
     );
@@ -11,126 +10,68 @@ export function denglureturn() {
 //导出轮播图返回值
 export function lunboreturn() {
     return get("http://10.8.155.18:3000/banner?type=2");
-=======
-  return get(
-    "http://10.8.155.18:3000/login/cellphone?phone=18639389097&password=xiaofang"
-  );
-}
-//导出轮播图返回值
-export function lunboreturn() {
-  return get("http://10.8.155.18:3000/banner?type=2");
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //导出我喜欢的音乐列表返回值
 export function likelistreturn() {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/likelist?uid=1553202823", {
         withCredentials: true
     });
-=======
-  return get("http://10.8.155.18:3000/likelist?uid=1553202823", {
-    withCredentials: true
-  });
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 // //歌曲详情
 export function likedatilereturn(a) {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/song/detail?ids=" + a);
-=======
-  return get("http://10.8.155.18:3000/song/detail?ids=" + a);
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 //歌曲url
 
 export function likedatileurl(id) {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/song/url?id=" + id);
-=======
-  return get("http://10.8.155.18:3000/song/url?id=" + id);
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //导出我收藏的MV
 export function collectreturn() {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/mv/sublist", {
         withCredentials: true
     });
-=======
-  return get("http://10.8.155.18:3000/mv/sublist", {
-    withCredentials: true
-  });
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //mv详情
 export function colloecdatilereturn(ids) {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/video/url?id=" + ids);
-=======
-  return get("http://10.8.155.18:3000/video/url?id=" + ids);
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //导出排行榜返回数据
 export function paihangbangreturn() {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/top/list?idx=1");
-=======
-  return get("http://10.8.155.18:3000/top/list?idx=1");
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //导出推荐视频数据返回值
 export function videoreturn() {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/mv/first?limit=5");
 }
 //导出推荐视频数据详情
 export function videoDetalreturn(id) {
     return get("http://10.8.155.18:3000/mv/url?id=" + id);
-=======
-  return get("http://10.8.155.18:3000/mv/first?limit=5");
-}
-//导出推荐视频数据详情
-export function videoDetalreturn(id) {
-  return get("http://10.8.155.18:3000/mv/url?id=" + id);
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //导出最近播放音乐返回值
 export function broadcastreturn() {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/user/record?uid=1553202823&type=1");
-=======
-  return get("http://10.8.155.18:3000/user/record?uid=1553202823&type=1");
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //导出最近播放歌曲详情
 export function broadcastdetal(id) {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/song/detail?ids=" + id);
-=======
-  return get("http://10.8.155.18:3000/song/detail?ids=" + id);
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //导出最近播放音乐详情
 export function broadcastdetalreturn(id) {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/song/url?id=" + id);
-=======
-  return get("http://10.8.155.18:3000/song/url?id=" + id);
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //根据id获取音乐url，可传多个，逗号隔开
 export function getmusicurl(id) {
-<<<<<<< HEAD
     return get("http://10.8.155.18:3000/song/url?id=" + id + "&br=320000");
 }
 //每日推荐歌曲
@@ -152,50 +93,29 @@ export function remengedanreturn() {
 //导出我收藏的歌单信息
 export function shoucanggedanreturn() {
     return get("http://10.8.155.18:3000/user/playlist?uid=1553202823");
-=======
-  return get("http://10.8.155.18:3000/song/url?id=" + id + "&br=320000");
-}
-//每日推荐歌曲
-export function meirituijianreturn() {
-  return get("http://10.8.155.18:3000/recommend/songs", {
-    withCredentials: true
-  });
-}
-//每日推荐歌单
-export function tuijiangedanreturn() {
-  return get("http://10.8.155.18:3000/recommend/resource", {
-    withCredentials: true
-  });
-}
-//首页热门歌单推荐
-export function remengedanreturn() {
-  return get("http://10.8.155.18:3000/top/playlist?limit=12");
-}
-//导出我收藏的歌单信息
-export function shoucanggedanreturn() {
-  return get("http://10.8.155.18:3000/user/playlist?uid=1553202823");
->>>>>>> 4373e9c4a09345012a10f738fd77fe2cd37989ed
 }
 
 //歌单详情
 export function gedanxiangqingreturn(id) {
-  return get("http://10.8.155.18:3000/playlist/detail?id=" + id);
+    return get("http://10.8.155.18:3000/playlist/detail?id=" + id);
 }
 
 //搜索接口 获取最终搜索的详情，拿着id调用获取音乐url
 export function sousuojiekou(key) {
-  return get("http://10.8.155.18:3000/search?keywords=" + key);
+    return get("http://10.8.155.18:3000/search?keywords=" + key);
 }
 
 //搜索建议调用此接口 , 传入搜索关键词可获得搜索建议，搜索结果同时包含单曲，歌手，歌单，mv信息。可选:type传mobile返回移动端数据
 export function search(key) {
-  return get(
-    "http://10.8.155.18:3000/search/suggest?keywords=" + key + "&type=mobile"
-  );
+    return get(
+        "http://10.8.155.18:3000/search/suggest?keywords=" +
+            key +
+            "&type=mobile"
+    );
 }
 //收藏/取消收藏歌单  t : 类型,1:收藏,2:取消收藏 id : 歌单 id
 export function gedanshoucang(t, id) {
-  return get(`http://10.8.155.18:3000/playlist/subscribe?t=${t}&id=${id}`, {
-    withCredentials: true
-  });
+    return get(`http://10.8.155.18:3000/playlist/subscribe?t=${t}&id=${id}`, {
+        withCredentials: true
+    });
 }
